1530ff3b924febfa40e0126cbad4064707fceef3
JarlBurget <jarl-ivor.burget@voco.ee>
2025-04-01 09:08:09 +0300

    esmane

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import Header from "./components/Header";
+import Meals from "./components/Meals"; 
+
 const App = () => {
- return (
+  return (
     <>
-     <h1>Food Order App</h1>
+      <Header />
+      <h1>Food Order App</h1>
+      <Meals /> 
     </>
   );
-}
+};
 
 export default App;
--- src/components/Meals.js
+++ src/components/Meals.js
@@ -2,7 +2,7 @@ const Meals = () => {
     return (
         <ul id="meals">
             { 
-                // list of meals
+               <h2>Create list of meals, using fetch data from backend</h2>
             }
         </ul>
     )
