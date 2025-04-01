90295b72cb8a3bb53b21cbe07134c7aa6c4d7b22
JarlBurget <jarl-ivor.burget@voco.ee>
2025-04-01 09:32:07 +0300

    Ylesanne 3

--- src/components/Meals.js
+++ src/components/Meals.js
@@ -1,11 +1,14 @@
-const Meals = () => {
-    return (
-        <ul id="meals">
-            { 
-               <h2>Create list of meals, using fetch data from backend</h2>
-            }
-        </ul>
-    )
-}
+import { useEffect } from "react";
 
-export default Meals
+const Meals = () => {
+    useEffect(() => {
+        fetch("http://localhost:3001/meals")
+            .then((response) => response.json())
+            .then((data) => {
+                console.log("Fetched meals:", data); // Kuvab andmed konsoolis
+            })
+            .catch((error) => console.error("Error fetching meals:", error));
+    }, []);
+};
+
+export default Meals;
