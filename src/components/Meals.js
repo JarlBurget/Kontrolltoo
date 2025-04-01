b47536c92e1cf5f54336e6f7f84d80ad2dcb5c8d
JarlBurget <jarl-ivor.burget@voco.ee>
2025-04-01 09:43:16 +0300

    Ylesanne 4

--- src/components/Meals.js
+++ src/components/Meals.js
@@ -1,14 +1,27 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
+import MealItem from "./MealItem";
 
 const Meals = () => {
+    const [meals, setMeals] = useState([]);
+
     useEffect(() => {
         fetch("http://localhost:3001/meals")
             .then((response) => response.json())
             .then((data) => {
-                console.log("Fetched meals:", data); // Kuvab andmed konsoolis
+                console.log("Fetched meals:", data);
+                setMeals(data); 
             })
             .catch((error) => console.error("Error fetching meals:", error));
     }, []);
+
+    return (
+        <ul id="meals">
+            {}
+            {meals.map((meal) => (
+                <MealItem key={meal.id} meal={meal} />
+            ))}
+        </ul>
+    );
 };
 
 export default Meals;
